655439504961b92724ce7c35af4474f46fd9c448
juliet26 <36687161+juliet26@users.noreply.github.com>
2018-07-21 22:14:13 -0500

    Diseño
    Programacion botones de inicio

---
+++ Navegacion/Navegacion/View/Home.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navegacion.View
+{
+	
+	public partial class Home : ContentPage
+	{
+		public Home ()
+		{
+			InitializeComponent ();
+            BackgroundImage = "Fondo1.jpeg";
+        }
+    }
+}
--- Navegacion/Navegacion/obj/Debug/netstandard2.0/View/IncidenciaRPage.xaml.g.cs
+++ Navegacion/Navegacion/obj/Debug/netstandard2.0/View/IncidenciaRPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Navegacion.View {
     public partial class IncidenciaRPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.DatePicker Date;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(IncidenciaRPage));
+            Date = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.DatePicker>(this, "Date");
         }
     }
 }
